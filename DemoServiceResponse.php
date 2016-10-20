277d6f8c0b91f10cdc068d0ab87a80132357a884
Andy Kirkham <andy@spiders-lair.com>
2016-10-20 09:56:48 +0100

    Add more missing functions

--- DemoServiceResponse.php
+++ DemoServiceResponse.php
@@ -4,9 +4,25 @@ namespace PhpScotland2016\Demo\Service\Interfaces;
 
 class DemoServiceResponse extends \Yaf_Response_Abstract
 {
+	public function __construct() {
+		parent::__construct();
+		$this->_header = array();
+	}
+
 	public function setHeader($header, $value) {
 		$this->_header[$header] = $value;
 		return $this;
+	}
+
+	public function getHeader($header) {
+		if(array_key_exists($header, $this->_header)) {
+			return $this->_header[$header];
+		}
+		return null;
+	}
+
+	public function getHeaders() {
+		return $this->_header;
 	}
 }
 
