6e42adb87ca4b396ef620a6953d8818a6f52fc39
Andy Kirkham <andy@spiders-lair.com>
2016-10-20 09:28:06 +0100

    Missing Dollar, less C, more PHP!

--- DemoServiceInterface.php
+++ DemoServiceInterface.php
@@ -9,6 +9,6 @@ interface DemoServiceInterface
 	 * @param \Yaf_Request_Http
 	 * @return \Yaf_Response_Abstract
 	 */
-	public function handleRequest(\Yaf_Request_Http request);
+	public function handleRequest(\Yaf_Request_Http $request);
 }
 
