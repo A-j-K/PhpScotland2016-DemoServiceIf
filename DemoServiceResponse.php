4b7dc684fa08d56a6109d54ca315c84cabaa568e
Andy Kirkham <andy@spiders-lair.com>
2016-10-20 09:52:49 +0100

    Add setHeader method, oddly, doesn't exist in abstract even thou the docs say otherwise

--- DemoServiceResponse.php
+++ DemoServiceResponse.php
@@ -3,5 +3,10 @@
 namespace PhpScotland2016\Demo\Service\Interfaces;
 
 class DemoServiceResponse extends \Yaf_Response_Abstract
-{}
+{
+	public function setHeader($header, $value) {
+		$this->_header[$header] = $value;
+		return $this;
+	}
+}
 
