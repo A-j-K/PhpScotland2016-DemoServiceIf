223d2017d5413e63ab5c4efc0000894f4561fd87
Andy Kirkham <andy.kirkham@logicnow.com>
2016-10-20 21:28:44 +0100

    Add extra methods to response object

--- DemoServiceResponse.php
+++ DemoServiceResponse.php
@@ -4,9 +4,15 @@ namespace PhpScotland2016\Demo\Service\Interfaces;
 
 class DemoServiceResponse extends \Yaf_Response_Abstract
 {
-	public function __construct() {
+	public function __construct($json = "{}") {
 		parent::__construct();
 		$this->_header = array();
+		if(is_array($json)) {
+			$this->setBodyFromArray($json);
+		{
+		else {
+			$this->setBody($json);
+		}
 	}
 
 	public function setHeader($header, $value) {
@@ -23,6 +29,18 @@ class DemoServiceResponse extends \Yaf_Response_Abstract
 
 	public function getHeaders() {
 		return $this->_header;
+	}
+
+	public function getJson() {
+		return $this->getBody();
+	}
+
+	public function getBodyAsArray() {
+		return json_decode($this->getBody(), true);
+	}
+
+	public function setBodyFromArray(array $arr) {
+		$this->setBody(json_encode($arr, JSON_FORCE_OBJECT));
 	}
 }
 
